9217d3d54a39aef09cc78fd1baab64a2a5ef90bf
rinchaekim <xxchrn@gmail.com>
2023-06-01 13:26:08 +0900

    Initial commit

---
+++ src/main/java/com/tsg/sjss/SjssApplication.java
@@ -0,0 +1,13 @@
+package com.tsg.sjss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsg/sjss/SjssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsg.sjss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
